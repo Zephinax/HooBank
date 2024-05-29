ce62600200d762f3afb894d131b1863490d23915
Zephinax <Zephinax@gmail.com>
2024-05-29 17:47:20 +0330

    ♾️

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img src={logo} alt='hoobank' className='w-[124px] h-[32px]' />
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+        {navLinks.map((nav, i) => (
+          <li
+            key={nav.id}
+            className={`font-poppins hover:text-secondary duration-200 ease-linear transition-all font-normal cursor-pointer text-[16px] ${
+              i === navLinks.length - 1 ? 'mr-0' : 'mr-10'
+            } text-white mr-10`}>
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+      <div className='sm:hidden flex flex-1 justify-end items-center'>
+        <img
+          src={toggle ? close : menu}
+          alt='menu'
+          className='w-[28px] h-[28px] object-contain'
+          onClick={() => setToggle((previous) => !previous)}
+        />
+        <div
+          className={`${
+            toggle ? 'flex' : 'hidden'
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+          <ul className='list-none flex flex-col justify-end items-center flex-1'>
+            {navLinks.map((nav, i) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                  i === navLinks.length - 1 ? 'mr-0' : 'mb-4'
+                } text-white mr-10`}>
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
