306e324c5af2c2f460b38c379b98af905d797262
Zephinax <Zephinax@gmail.com>
2024-05-29 18:45:20 +0330

    feat: translate billing

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { apple, bill, google } from '../assets'
-import styles, { layout } from '../style'
+import React from 'react';
+import { apple, bill, google } from '../assets';
+import styles, { layout } from '../style';
 
 const Billing = () => {
   return (
@@ -11,15 +11,19 @@ const Billing = () => {
           alt='billing'
           className='w-[100%] h-[100%] relative z-[5]'
         />
-        <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient'/>
-        <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient'/>
+        <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient' />
+        <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient' />
       </div>
-      <div className={layout.sectionInfo}>
-        <h2 className={styles.heading2}>Easily control your <br className='sm:block hidden'/> billing & invoicing.</h2>
+      <div dir='rtl' className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          صورت حساب خود را <br className='sm:block hidden' /> به راحتی کنترل
+          کنید
+        </h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          Elit enim sed massa etiam.
-          Mauris eu adipiscing ultrices ametodio aenean neque.
-          Fusce ipsum orci rhoncus aliporttitor integer platea placerat.
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+          از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
+          سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای
+          متنوع با هدف بهبود ابزارهای کاربردی می باشد.
         </p>
         <div className='flex flex-row flex-wrap sm:mt-10 mt-6'>
           <img
@@ -33,9 +37,9 @@ const Billing = () => {
             className='w-[128px] h-[42px] object-contain cursor-pointer'
           />
         </div>
-      </div>  
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Billing
+export default Billing;
