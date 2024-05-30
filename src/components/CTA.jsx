9e494ca1ebe6ae39f99e406f6adc8c590c4241e3
Zephinax <Zephinax@gmail.com>
2024-05-30 20:24:50 +0330

    fix: some tweaks and style fix

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -7,14 +7,16 @@ const CTA = () => {
     <section
       dir='rtl'
       className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
-      <div className='flex-1 flex flex-col'>
-        <h2 className={styles.heading2}>خدمات مارا هم اکنون امتحان کنید!</h2>
-        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      <div className='flex-1 flex flex-col sm:pl-8'>
+        <h2 className={`${styles.heading2} text-center md:text-right`}>
+          خدمات مارا اکنون امتحان کنید!
+        </h2>
+        <p className={`${styles.paragraph} max-w-[595px] text-center mt-5`}>
           همه چیزهایی که برای پذیرش پرداخت های کارتی و رشد کسب و کار خود در هر
           نقطه از کره زمین نیاز دارید.
         </p>
       </div>
-      <div className={`${styles.flexCenter} sm:ml-10 ml-0`}>
+      <div className={`${styles.flexCenter} mt-8 sm:mt-0 sm:ml-10 ml-0`}>
         <Button />
       </div>
     </section>
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -14,8 +14,21 @@ const Stats = () => {
           className={`flex-1 flex justify-center gap-1 items-center flex-row m-3`}>
           <h4
             dir='ltr'
-            className='font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white '>
-            <CountUp start={0} end={parseInt(stat.value, 10)} duration={10} />+
+            className={`font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white ${
+              (parseInt(stat.value, 10) > 999 && 'min-w-[6ch]') ||
+              (parseInt(stat.value, 10) > 99 && 'min-w-[4ch]') ||
+              (parseInt(stat.value, 10) > 0 && 'min-w-[3ch]')
+            } `}>
+            <CountUp
+              start={0}
+              end={parseInt(stat.value, 10)}
+              duration={
+                (parseInt(stat.value, 10) > 1000 && 0.01) ||
+                (parseInt(stat.value, 10) > 200 && 20) ||
+                (parseInt(stat.value, 10) > 0 && 100)
+              }
+            />
+            +
           </h4>
           <p className='text-nowrap font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient uppercase ml-3'>
             {stat.title}
