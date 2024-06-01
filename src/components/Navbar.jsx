71dc2c0401f2197915c646c08c64086e90592b01
Zephinax <Zephinax@gmail.com>
2024-06-01 21:11:09 +0330

    feat: new nav toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { close, logo, menu } from '../assets';
+import { logo } from '../assets';
 import { navLinks } from '../constants';
+import './NavToggle.css';
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
@@ -20,12 +21,18 @@ const Navbar = () => {
         ))}
       </ul>
       <div className='sm:hidden flex flex-1 justify-end items-center'>
-        <img
-          src={toggle ? close : menu}
-          alt='menu'
-          className='w-[28px] h-[28px] object-contain'
-          onClick={() => setToggle((previous) => !previous)}
-        />
+        <label className='hamburger'>
+          <input
+            checked={toggle}
+            onChange={() => setToggle((previous) => !previous)}
+            type='checkbox'></input>
+          <svg viewBox='0 0 32 32'>
+            <path
+              className='line line-top-bottom'
+              d='M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22'></path>
+            <path className='line' d='M7 16 27 16'></path>
+          </svg>
+        </label>
         <div
           className={`${
             toggle ? 'flex' : 'hidden'
